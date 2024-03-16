9c7c12737ef37023f14b8c65acb9852d63d4b787
namsan01 <namsan2934@gmail.com>
2024-03-16 20:18:26 +0900

    notionApi 수정

--- src/api/notionApi.ts
+++ src/api/notionApi.ts
@@ -1,8 +1,5 @@
 import { ResultItem } from "../types/Project";
 
-const TOKEN = process.env.React_APP_Notion_Key;
-const DATABASE_ID = process.env.React_APP_Notion_DATA;
-
 interface ResponseData {
   object: string;
   results: ResultItem[];
@@ -11,29 +8,8 @@ interface ResponseData {
   type: string;
 }
 
-const options = {
-  method: "POST",
-  headers: {
-    Accept: "application/json",
-    "Notion-Version": "2022-06-28",
-    "content-type": "application/json",
-    Authorization: `Bearer ${TOKEN}`,
-  },
-  body: JSON.stringify({
-    sorts: [
-      {
-        property: "title",
-        direction: "ascending",
-      },
-    ],
-  }),
-};
-
 export const getData = async (): Promise<ResultItem[]> => {
-  const res = await fetch(
-    `https://cors-anywhere.herokuapp.com/https://api.notion.com/v1/databases/${DATABASE_ID}/query`,
-    options
-  );
+  const res = await fetch(`https://portfolio.corcor37.workers.dev/`);
 
   const result: ResponseData = await res.json();
 
